347d057e6292cbd51c84efc35861aea20234c88a
matluz <matluz994@gmail.com>
2025-01-30 16:59:22 -0300

    add task 1 from practical exercise

---
+++ exercícios-aula2/GestaoDeProdutos/GestaoDeProdutos.Services.Api/Controllers/EstoquesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeProdutos.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstoquesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ exercícios-aula2/GestaoDeProdutos/GestaoDeProdutos.Services.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeProdutos.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
